95cfb950ffd71ef3694ca4835682a93a1a83a356
hoppsik <120632594+hoppsikCZ@users.noreply.github.com>
2024-10-15 22:45:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ t.setup(800, 600)
 keep_going = True
 while keep_going:
     t.reset()
-    choice = t.numinput("Vyber možnost", "1 - Náhodné čáry\n2 - Čtvercová spirála\n3 - Klikání\n4 - D", 1, 1, 4)
+    choice = t.numinput("Vyber možnost",
+                        "1 - Náhodné čáry\n2 - Čtvercová spirála\n3 - Náhodné spirály\n4 - Klikání (nelze opustit)", 1,
+                        1, 4)
     if choice == 1:
         tf.random_lines()
     elif choice == 2:
@@ -14,10 +16,14 @@ while keep_going:
     elif choice == 3:
         tf.random_spirals()
     elif choice == 4:
-        print("D")
+        t.onscreenclick(tf.go_to_click)
+        t.mainloop()
+        break
     else:
         print("Neplatná možnost")
 
-    exit_choice = t.textinput("Chceš pokračovat?", "(ano/ne)")
-    if exit_choice.lower() == "ne" or exit_choice.lower() == "n":
-        keep_going = False
+    keep_going = False
+    exit_choice = t.textinput("Chceš pokračovat?", "(ano/NE)")
+    if exit_choice is not None:
+        if exit_choice.lower() == "ano" or exit_choice.lower() == "a":
+            keep_going = True
--- turtle_functions.py
+++ turtle_functions.py
@@ -1,8 +1,12 @@
 import turtle as t
 import random
+from math import sin, cos, pi
 
 
 def random_lines():
+    """
+    Draws a specified number of random lines.
+    """
     count = t.numinput("Počet čar", "Zadej počet čar", 1, 1, 100)
     for i in range(int(count)):
         t.penup()
@@ -13,22 +17,45 @@ def random_lines():
 
 
 def random_spirals():
+    """
+    Draws a specified number of random spirals.
+    """
     count = t.numinput("Počet spirál", "Zadej počet spirál", 1, 1, 100)
-    for i in range(int(count)):
+    for _ in range(int(count)):
         t.penup()
         t.color(random.random(), random.random(), random.random())
         t.goto(random.randint(-200, 200), random.randint(-200, 200))
+        start_angle = random.randint(0, 360)
         t.pendown()
-        side_length = 10
-        for a in range(random.randint(3, 7)):
-            t.forward(side_length)
-            t.right(90)
-            side_length += 10
+
+        d = random.randint(10, 50)
+        cur_r = 0
+        start_x, start_y = t.position()
+
+        for _ in range(random.randint(2, 5)):
+            for a in range(1, 360, 4):
+                r = cur_r + d * a / 360.0
+                a_rad = (a + start_angle) * pi / 180.0
+                y = start_y + r * sin(a_rad)
+                x = start_x + r * cos(a_rad)
+                t.goto(x, y)
+            cur_r += d
 
 
 def square_spiral():
+    """
+    Draws a square spiral.
+    """
     side_length = 10
     for _ in range(20):
         t.forward(side_length)
         t.right(90)
         side_length += 10
+
+
+def go_to_click(x, y):
+    """
+    Moves the turtle to the click position.
+    """
+    t.color(random.random(), random.random(), random.random())
+    t.goto(x, y)
